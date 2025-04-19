b2d5931f0750d85f169d293f6e66fcba50cf3744
Niv <nivb49@gmail.com>
2025-04-19 21:21:15 +0300

    vpc

--- vpc.tf
+++ vpc.tf
@@ -12,14 +12,14 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = var.availability_zone_public
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = var.availability_zone_private
 }
 
 #route table
