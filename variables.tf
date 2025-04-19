f06cc7a39448f7ab8806f57675dd410250c50f65
Niv <nivb49@gmail.com>
2025-04-19 21:31:03 +0300

    variables

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "region" {
   default = "us-east-1"
 }
 
+#ec2
 variable "ami_id" {
   description = "The AMI ID to use for the EC2 instances"
   type        = string
@@ -12,4 +13,17 @@ variable "instance_type" {
   description = "The instance type for the EC2 instances"
   type        = string
   default     = "t2.micro"
+}
+
+#vpc
+variable "availability_zone_public" {
+  description = "The Availability Zone for the public Instance"
+  type = string
+  default = "us-east-1a"
+}
+
+variable "availability_zone_private" {
+  description = "The Availability Zone for the private Instance"
+  type = string
+  default = "us-east-1b"
 }
