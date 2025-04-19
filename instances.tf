667bb5e535a5e88e825fd401740415aa6145a10d
Niv <nivb49@gmail.com>
2025-04-19 20:09:19 +0300

    added variables

--- instances.tf
+++ instances.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "web_instance" {
-  ami           = "ami-0e449927258d45bc4" 
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.web_sg.id]
 }
 
 resource "aws_instance" "db_instance" {
-  ami           = "ami-0e449927258d45bc4" 
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
   subnet_id     = aws_subnet.private_subnet.id
   security_groups = [aws_security_group.db_sg.id]
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "region" {
   default = "us-east-1"
 }
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instances"
+  type        = string
+  default = "ami-0e449927258d45bc4"
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instances"
+  type        = string
+  default     = "t2.micro"
+}
