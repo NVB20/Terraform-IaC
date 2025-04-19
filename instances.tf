96f11aafc7bc776a9d0fa47a5bd8d739270e1df0
Niv <nivb49@gmail.com>
2025-04-19 21:30:40 +0300

    instances

--- instances.tf
+++ instances.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_instance" {
   ami           = var.ami_id
-  instance_type = var.intype
-  subnet_id     = aws_subnet.public_sstance_ubnet.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.web_sg.id]
   tags = {
     Name = "web_instance"
